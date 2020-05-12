b6dc9a653a28be92dbca7a6622cb26be71adc9b3
Michael Beknazarov <michaelbeknazarov@gmail.com>
2020-05-12 11:12:22 +0300

    add metadata for publishing to Gradle Plugin Portal

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,3 +22,18 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-gradle-plugin:2.2.6.RELEASE")
     implementation("io.spring.gradle:dependency-management-plugin:1.0.9.RELEASE")
 }
+
+pluginBundle {
+    website = "https://github.com/jvadev/gradle-kotlin-common-plugin"
+    vcsUrl = "https://github.com/jvadev/gradle-kotlin-common-plugin.git"
+    tags = listOf("kotlin", "ktlint")
+}
+
+gradlePlugin
+    .plugins
+    .find { it.name == "gradle-kotlin-common-plugin" }!!
+    .apply {
+        id = "com.jvadev.gradle-kotlin-common-plugin"
+        displayName = "A plugin that sets up kotlin and checks code style in your project"
+        description = "A plugin that sets up kotlin dependencies, plugins, build settings and checks official code style"
+    }
